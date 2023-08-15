54057b306d0a30a95420fe999fa1b5f59c6dd279
Harshwardhan Domal <harshdomal@gmail.com>
2023-08-16 00:34:42 +0530

    complete challenge 8

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutThisContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactUsContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  console.log(req.body.composeInput);
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
